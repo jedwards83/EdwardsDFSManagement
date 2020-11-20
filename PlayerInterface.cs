4dc72b7a54a6ceef28b5cb036318166469d8accc
Jonathan Edwards <Jonathan.edwardsctc@gmail.com>
2020-11-20 07:49:19 -0500

    Building Menu Options before building Switches

--- PlayerInterface.cs
+++ PlayerInterface.cs
@@ -8,11 +8,33 @@ namespace EdwardsManagement
     class PlayerInterface
     {
         const string path = "PlayerDB.json";
-        private PlayerManager _playerManager = new PlayerManager();
+        private EdwardsManager _edwardsManager = new EdwardsManager();
 
         public PlayerInterface()
         {
-            _playerManager.LoadContacts(path);
+            _edwardsManager.LoadPlayers(path);
+        }
+        //build loop interface
+        internal void StartPlayerInterface()
+        {
+            var shouldExit = false;
+            while (!shouldExit)
+            {
+                Console.WriteLine("What player are you looking for?");
+                //menu options
+                //exit
+                Console.WriteLine("0. Exit Player Manager");
+                //list contacts
+                Console.WriteLine("1. List all players");
+                //search contacts
+                Console.WriteLine("2. Search by name");
+                //view contact
+                Console.WriteLine("3. Search by Salary");
+                //add contact
+                Console.WriteLine("4. Find all Below Salary");
+
+                var action = Console.ReadLine();
+            }
         }
     }
 }
