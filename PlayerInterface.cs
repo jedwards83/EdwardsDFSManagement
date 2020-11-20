43c5a11c045a33464f0168f11d70a45f7b2ea8f0
Jonathan Edwards <Jonathan.edwardsctc@gmail.com>
2020-11-20 00:01:37 -0500

    Added Player interface. Starting to build interface actions

---
+++ PlayerInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace EdwardsManagement
+{
+    class PlayerInterface
+    {
+        const string path = "PlayerDB.json";
+        private PlayerManager _playerManager = new PlayerManager();
+
+        public PlayerInterface()
+        {
+            _playerManager.LoadContacts(path);
+        }
+    }
+}
